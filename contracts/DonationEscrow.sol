8ee7687a9322f7b481b94f031550b7dd82a5f6c1
Christopher Chibueze Obasi <167867797+Doublegee5@users.noreply.github.com>
2025-06-10 22:58:50 +0100

    Update DonationEscrow.sol

--- contracts/DonationEscrow.sol
+++ contracts/DonationEscrow.sol
@@ -1,1 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+contract DonationEscrow {
+    address public student;
+    address public donor;
+    uint256 public totalAmount;
+    uint256 public milestoneAmount;
+    uint8 public milestones;
+    uint8 public currentMilestone;
+
+    constructor(
+        address _student,
+        address _donor,
+        uint8 _milestones
+    ) payable {
+        require(msg.value > 0, "No funds sent");
+        student = _student;
+        donor = _donor;
+        totalAmount = msg.value;
+        milestones = _milestones;
+        milestoneAmount = msg.value / _milestones;
+        currentMilestone = 0;
+    }
+
+    function verifyMilestone() external {
+        // Will later integrate Chainlink Functions here
+        require(currentMilestone < milestones, "All milestones completed");
+        currentMilestone++;
+        payable(student).transfer(milestoneAmount);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
