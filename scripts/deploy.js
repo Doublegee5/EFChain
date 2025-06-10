0fdad634c44fc6c1511120b59fceab68dc8b5dd2
Christopher Chibueze Obasi <167867797+Doublegee5@users.noreply.github.com>
2025-06-10 23:03:08 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,13 @@
+const hre = require("hardhat");
 
+async function main() {
+  const SBT = await hre.ethers.getContractFactory("EFchainStudentSBT");
+  const sbt = await SBT.deploy();
+  await sbt.deployed();
+  console.log("EFchainStudentSBT deployed to:", sbt.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
