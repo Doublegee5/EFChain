b6f434aebf19022e4986a0a901fa5cac657b2f8a
Christopher Chibueze Obasi <167867797+Doublegee5@users.noreply.github.com>
2025-06-10 22:56:04 +0100

    Update EfchainStudentSBT.sol

--- contracts/EfchainStudentSBT.sol
+++ contracts/EfchainStudentSBT.sol
@@ -1,1 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EFchainStudentSBT is ERC721URIStorage, Ownable {
+    uint256 public nextTokenId;
+    mapping(address => bool) public hasMinted;
+
+    constructor() ERC721("EFchain Student SBT", "EFSBT") {}
+
+    function mintSBT(string memory tokenURI) external {
+        require(!hasMinted[msg.sender], "Already minted");
+        uint256 tokenId = nextTokenId;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        hasMinted[msg.sender] = true;
+        nextTokenId++;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override {
+        require(from == address(0), "SBTs are non-transferable");
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+}
