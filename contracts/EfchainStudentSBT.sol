92af665a0b346a3b492766f7da51eca74dc0e700
Christopher Chibueze Obasi <167867797+Doublegee5@users.noreply.github.com>
2025-06-17 14:06:59 +0100

    Update EfchainStudentSBT.sol

--- contracts/EfchainStudentSBT.sol
+++ contracts/EfchainStudentSBT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20; // Use ^0.8.20 to match OpenZeppelin
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -8,7 +8,7 @@ contract EFchainStudentSBT is ERC721URIStorage, Ownable {
     uint256 public nextTokenId;
     mapping(address => bool) public hasMinted;
 
-    constructor(address initialOwner) ERC721("EFchain Student SBT", "EFSBT") Ownable(initialOwner) {}
+    constructor() ERC721("EFchain Student SBT", "EFSBT") Ownable(msg.sender) {}
 
     function mintSBT(string memory tokenURI) external {
         require(!hasMinted[msg.sender], "Already minted");
@@ -19,13 +19,10 @@ contract EFchainStudentSBT is ERC721URIStorage, Ownable {
         nextTokenId++;
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId,
-        uint256 batchSize
-    ) internal override {
+    // Prevent transfers - SBT logic (OpenZeppelin 5.x uses _update)
+    function _update(address to, uint256 tokenId, address auth) internal override returns (address) {
+        address from = super._update(to, tokenId, auth);
         require(from == address(0), "SBTs are non-transferable");
-        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+        return from;
     }
 }
