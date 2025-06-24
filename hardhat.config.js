e0fbfb1224c3181ef0dcaff53250c3bc86de285a
Christopher Chibueze Obasi <167867797+Doublegee5@users.noreply.github.com>
2025-06-24 12:04:53 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.19",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID", // or Alchemy endpoint
+      accounts: ["0xYOUR_PRIVATE_KEY"]
     }
   }
 };
