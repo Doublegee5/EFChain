4c7acfaf1415641dc0fe1e5815a520f4f64d9368
Christopher Chibueze Obasi <167867797+Doublegee5@users.noreply.github.com>
2025-06-25 19:04:27 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,12 @@
-require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
+  solidity: "0.8.19",
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+      url: process.env.SEPOLIA_RPC_URL,
       accounts: [process.env.PRIVATE_KEY]
     }
-  },
-  solidity: "0.8.19",
+  }
 };
