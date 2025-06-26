bbda09e6733db7635758f40281e25b41dd6e4a0d
Christopher Chibueze Obasi <167867797+Doublegee5@users.noreply.github.com>
2025-06-26 22:36:50 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.19",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`${process.env.PRIVATE_KEY}`],
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
