46223cd5304b6a59ae3b216192cca8ac0788e7c5
Christopher Chibueze Obasi <167867797+Doublegee5@users.noreply.github.com>
2025-06-28 02:21:42 +0100

    Update EfchainStudentSBT.sol

--- contracts/EfchainStudentSBT.sol
+++ contracts/EfchainStudentSBT.sol
@@ -4,22 +4,11 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// Chainlink Functions imports
-import {FunctionsClient} from "@chainlink/functions/contracts/dev/0_0_4/FunctionsClient.sol";
-import {FunctionsRequest} from "@chainlink/functions/contracts/dev/0_0_4/libraries/FunctionsRequest.sol";
-
-contract EFchainStudentSBT is ERC721URIStorage, Ownable, FunctionsClient {
-    using FunctionsRequest for FunctionsRequest.Request;
-
+contract EFchainStudentSBT is ERC721URIStorage, Ownable {
     uint256 public nextTokenId;
     mapping(address => bool) public hasMinted;
-    bytes32 public latestRequestId;
-    string public latestResponse;
 
-    event VerificationRequested(bytes32 requestId);
-    event VerificationCompleted(string result);
-
-    constructor(address oracle) ERC721("EFchain Student SBT", "EFSBT") Ownable(msg.sender) FunctionsClient(oracle) {}
+    constructor() ERC721("EFchain Student SBT", "EFSBT") Ownable(msg.sender) {}
 
     function mintSBT(string memory tokenURI) external {
         require(!hasMinted[msg.sender], "Already minted");
@@ -30,26 +19,7 @@ contract EFchainStudentSBT is ERC721URIStorage, Ownable, FunctionsClient {
         nextTokenId++;
     }
 
-    function requestVerification(string memory milestone) public {
-        FunctionsRequest.Request memory req;
-        req.initializeRequestForInlineJavaScript("return args[0];");
-        string ;
-        args[0] = milestone;
-        req.setArgs(args);
-
-        // Adjust gas limit and don’t forget to fund your contract with LINK!
-        bytes32 assignedReqID = _sendRequest(req.encodeCBOR(), 300000);
-        latestRequestId = assignedReqID;
-        emit VerificationRequested(assignedReqID);
-    }
-
-    // Chainlink Functions fulfill function
-    function fulfillRequest(bytes32 requestId, bytes memory response, bytes memory /* err */) internal override {
-        latestResponse = string(response);
-        emit VerificationCompleted(latestResponse);
-    }
-
-    // Prevent transfers - SBT logic
+    // Prevent transfers - SBT logic (OpenZeppelin 5.x uses _update)
     function _update(address to, uint256 tokenId, address auth) internal override returns (address) {
         address from = super._update(to, tokenId, auth);
         require(from == address(0), "SBTs are non-transferable");
