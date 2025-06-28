ced60c6ece6b18f3b0322b70a3f794f2d8b47f9c
Christopher Chibueze Obasi <167867797+Doublegee5@users.noreply.github.com>
2025-06-28 02:36:26 +0100

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,7 @@ async function main() {
   const [deployer] = await hre.ethers.getSigners();
   console.log("Deploying contracts with:", deployer.address);
 
-  const oracleAddress = hre.ethers.utils.getAddress("0xCc5cA5A9dC7C1C1B2f38fCdA2517Da2e72eD0564");
+  const oracleAddress = hre.ethers.utils.getAddress("OxCc5cA5A9dC7C1C1B2f38fCdA2517Da2e72eD0564");
   const Contract = await hre.ethers.getContractFactory("EFchainStudentSBT");
   const contract = await Contract.deploy(oracleAddress);
 
